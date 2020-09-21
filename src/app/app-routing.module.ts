d8e18f2a279f4d2c871bc1d709724fd87e67c62c
Ahmed777Khalil <progeng_ahmed_khaliloutlook.com>
2020-09-21 08:17:08 +0200

    Adding New

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { ProductDetailsComponent } from './Components/product-details/product-details.component';
+import { ProductsListComponent } from './Components/products-list/products-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'details', component: ProductDetailsComponent },
+  { path: 'products/:searchText', component: ProductsListComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+ 
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,53 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { HomeComponent } from './Components/home/home.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule} from '@angular/material/button';
+import { MatTabsModule} from '@angular/material/tabs';
+import { AboutComponent } from './Components/about/about.component';
+import { ServicesComponent } from './Components/services/services.component';
+import { ProductsListComponent } from './Components/products-list/products-list.component';
+import { ProductDetailsComponent } from './Components/product-details/product-details.component';
+import { ContactUsComponent } from './Components/contact-us/contact-us.component';
 @NgModule({
+  exports: [
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTabsModule
+  ],
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    AboutComponent,
+    ServicesComponent,
+    ProductsListComponent,
+    ProductDetailsComponent,
+    ContactUsComponent
   ],
   imports: [
+    MDBBootstrapModule.forRoot(),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
